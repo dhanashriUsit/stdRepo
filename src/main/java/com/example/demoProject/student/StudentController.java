83b2cf6414caebd611ed3e118070133aba9500ec
dhanashriUsit <dhanashri.deore@usit.net.in>
2024-02-26 15:34:26 +0530

    cross origin

--- src/main/java/com/example/demoProject/student/StudentController.java
+++ src/main/java/com/example/demoProject/student/StudentController.java
@@ -1,8 +1,11 @@
 package com.example.demoProject.student;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@CrossOrigin
 @RestController
 public class StudentController {
 
