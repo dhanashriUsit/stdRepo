ca55e78f66923863a8125d0a3ef6b0e80069e253
dhanashriUsit <dhanashri.deore@usit.net.in>
2024-02-26 11:46:29 +0530

    demo Project Application

--- src/main/java/com/example/demoProject/DemoProjectApplication.java
+++ src/main/java/com/example/demoProject/DemoProjectApplication.java
@@ -2,12 +2,22 @@ package com.example.demoProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
 
 @SpringBootApplication
-public class DemoProjectApplication {
+public class DemoProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+    {
+    return builder.sources (DemoProjectApplication.class);
+    }
 
 }
