438b5e097ff34c000bd77a1ae0d5b6a9c8974934
dhanashriUsit <dhanashri.deore@usit.net.in>
2024-02-26 15:50:03 +0530

    cmt

--- src/main/java/com/example/demoProject/student/StudentController.java
+++ src/main/java/com/example/demoProject/student/StudentController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//@CrossOrigin(origins = "http://localhost:8082")
+@CrossOrigin
 @RestController
 public class StudentController {
 
