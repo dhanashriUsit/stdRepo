c678f9716835e9b18baeee25b6fc978eb69457c3
dhanashriUsit <dhanashri.deore@usit.net.in>
2024-02-27 11:45:02 +0530

    @Rep

--- src/main/java/com/example/demoProject/student/StdRepo.java
+++ src/main/java/com/example/demoProject/student/StdRepo.java
@@ -1,7 +1,8 @@
 package com.example.demoProject.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface StdRepo extends JpaRepository<Student, Integer> {
 
 }
